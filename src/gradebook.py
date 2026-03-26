1099f9997359d4449ee8a5355ba2e1180a197690
Queverity <pryor.baird@ucas-edu.net>
2026-03-26 02:42:26 +0000

    finished making pseudocode for gradebook

--- src/gradebook.py
+++ src/gradebook.py
@@ -10,7 +10,20 @@
         # if id, have user enter id, then iterate through catalog to see which id matches, then print that out
 
     # edit_student(self):
-        
+        # iterate through catalog and print out
+        # have user choose student
+        # ask user if they would like to edit name, id, add a grade, or remove a grade
+        # for name or id, just have user input something new
+        # for grades, call methods built into student class
+
     # add_student(self):
         # have user enter student name and id
-        # create student object and save
+        # create student object and save to csv
+
+    # find_average(self):
+        # go through catalog, and add each grade in each students list to one big list
+        # sum that list and divide by length to find average
+
+    # find_high_low(self):
+        # use lambda function to sort catalog list
+        # highest will be last index, lowest will be first index (sort based on average)
---
+++ src/user_interface.py
@@ -0,0 +1,4 @@
+# CB 1st User Interface
+
+# main_interface():
+    #
