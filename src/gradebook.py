67b7fe775e04dc3e96400faa8a8f0358dbc99db7
Queverity <pryor.baird@ucas-edu.net>
2026-03-26 02:20:56 +0000

    made some pseudocode for gradebook

--- src/gradebook.py
+++ src/gradebook.py
@@ -1,4 +1,16 @@
 # CB 1st Gradebook Class
 
 # class Gradebook():
-    
+    # def __init__(self, catalog = []):
+        # catalog is just a list
+
+    # search_student(self):
+        # ask user if they would like to search by name or id
+        # if name, have user enter name, then iterate through catalog to see which names match, and print those, along with more specific details
+        # if id, have user enter id, then iterate through catalog to see which id matches, then print that out
+
+    # edit_student(self):
+        
+    # add_student(self):
+        # have user enter student name and id
+        # create student object and save
